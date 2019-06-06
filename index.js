f3c12f8cee4a088e9a7569310e2057090dfe9b8d
Goohyun Jung <futuremaker019@gmail.com>
2019-06-06 16:10:59 +0400

    Rev.02

--- index.js
+++ index.js
@@ -14,75 +14,19 @@
 // 2-1. winner 정하기, (if 문 사용)
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
-function diceRandom1 () {
-
-  var num;
-
-  if (randomNumber1 === 1){
-    num = document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-  }
-  else if (randomNumber1 === 2) {
-    num = document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-  }
-  else if (randomNumber1 === 3) {
-    num = document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-  }
-  else if (randomNumber1 === 4) {
-    num = document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-  }
-  else if (randomNumber1 === 5) {
-    num = document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-  }
-  else {
-    num = document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-  }
-  return num;
-}
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+var dice1 = document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+var dice2 = document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
 
-function diceRandom2 () {
-
-  var num;
-
-  if (randomNumber2 === 1){
-    num = document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-  }
-  else if (randomNumber2 === 2) {
-    num = document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-  }
-  else if (randomNumber2 === 3) {
-    num = document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-  }
-  else if (randomNumber2 === 4) {
-    num = document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-  }
-  else if (randomNumber2 === 5) {
-    num = document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-  }
-  else {
-    num = document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-  }
-  return num;
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "★ Player1 wins";
+}  else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "♬ Player2 wins";
+} else {
+  document.querySelector("h1").innerHTML = "Draw ♣";
 }
 
-function winner() {
-
-  var whoWins =  null;
-
-  if (randomNumber1 > randomNumber2) {
-    whoWins = document.querySelector("h1").innerHTML = "Player 1 wins";
-  }
-  else if (randomNumber1 < randomNumber2) {
-    whoWins = document.querySelector("h1").innerHTML = "Player 2 wins";
-  }
-  else {
-    whoWins = document.querySelector("h1").innerHTML = "Draw";
-  }
-  return whoWins;
-}
-
-diceRandom1 ();
-diceRandom2 ();
-winner();
+document.write("hi there");
