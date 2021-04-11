abd908127cf68cb004b20cf38668c48f1be1ead9
ghjung <ghjung@simmssoft.com>
2021-04-11 22:55:45 +0900

    7.3.Redux applied

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,34 +4,39 @@ import TodoList from "../components/TodoList";
 import {TodoType} from "../types/todo";
 import {getTodosAPI} from "../lib/api/todo";
 import styled from "styled-components";
+import {wrapper} from "../store";
+import {todoActions} from "../store/todo";
 
 interface IProps {
   todos: TodoType[];
 }
 
-const todos : TodoType[] = [
-  {id:1, text:"마트 가서 장보기", color:"red", checked:false},
-  {id:2, text:"수학 숙제하기", color:"orange", checked:false},
-  {id:3, text:"코딩하기", color:"yellow", checked:true},
-  {id:4, text:"넥스트 공부하기", color:"green", checked:true},
-  {id:5, text:"요리 연습하기", color:"blue", checked:false},
-  {id:6, text:"분리수거 하기", color:"navy", checked:false},
-];
+// const todos : TodoType[] = [
+//   {id:1, text:"마트 가서 장보기", color:"red", checked:false},
+//   {id:2, text:"수학 숙제하기", color:"orange", checked:false},
+//   {id:3, text:"코딩하기", color:"yellow", checked:true},
+//   {id:4, text:"넥스트 공부하기", color:"green", checked:true},
+//   {id:5, text:"요리 연습하기", color:"blue", checked:false},
+//   {id:6, text:"분리수거 하기", color:"navy", checked:false},
+// ];
 
 
 const app: NextPage<IProps> = ({todos}) => {
   return <TodoList todos={todos}/>;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const {data} = await getTodosAPI();
-    return {props: {todos:data}};
-  } catch (e) {
-    console.log(e);
-    return {props: {todos:[]}};
+export const getServerSideProps = wrapper.getServerSideProps(
+  async ({store}) => {
+    console.log(store);
+    try {
+      const {data} = await getTodosAPI();
+      store.dispatch(todoActions.setTodo(data));
+      return {props: {todos:data}};
+    } catch (e) {
+      console.log(e);
+      return {props: {todos:[]}};
+    }
   }
-};
+);
 
 export default app;
-
--- store/todo.ts
+++ store/todo.ts
@@ -1,4 +1,5 @@
 import { TodoType } from "../types/todo";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 // 3. 항상 npm-module-or-app/reducer/ACTION_TYPE 형태의 action 타입을 가져야 합니다.
 // 액션타입 정의
@@ -29,7 +30,7 @@ const initialState: TodoReduxState = {
 // 리듀서
 export default function reducer(state = initialState, action: any) {
   switch (action.type) {
-    case SET_TODO_LIST:
+    case INIT_TODO_LIST:
       const newState = {...state, todos: action.payload};
       return newState;
     default:
