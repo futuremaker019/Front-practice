fb89791e627d7ba16b6f3d6c7bda4a424321ffd2
ghjung <ghjung@simmssoft.com>
2021-04-22 21:47:51 +0900

    10.1.2.common input component created

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,10 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import palette from "../../styles/palette"
+import CloseXIcon from "../../public/static/svg/modal/modal_colose_x_icon.svg"
+import Mailcon from "../../public/static/svg/auth/mail.svg"
+import PersonIcon from "../../public/static/svg/auth/person.svg"
+import OpenedEyeIcon from "../../public/static/svg/auth/opened-eye.svg"
+import CloseEyeIcon from "../../public/static/svg/auth/closed_eye.svg"
+import Input from "../common/Input";
 
 const Container = styled.div`
   width: 568px;
   height: 614px;
+  padding:32px;
   background-color: white;
   z-index: 11;
   
@@ -36,12 +43,89 @@ const Container = styled.div`
       top: 16px;
     }
   }
+  .sign-up-password-input-wrapper {
+    svg {
+      cursor:pointer;
+    }
+  }
 `;
 
 const SignUpModal: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [password, setPassword] = useState("");
+  const [hidePassword, setHidePassword] = useState(true);
+
+  // 이메일 주소 변경 시
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  // 이름 변경 시
+  const onChangeLastname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastname(event.target.value);
+  };
+
+  // 성 변경 시
+  const onChangeFirstname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstname(event.target.value);
+  };
+
+  // 비밀번호 변경 시
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  // 비밀번호 숨김 토글하기
+  const toggleHidePassword = () => {
+    setHidePassword(!hidePassword);
+  }
+
   return (
     <Container>
-      <CloseXIcon />
+      <CloseXIcon className="modal-close-x-icon" />
+      <div className="input-wrapper">
+        <Input 
+          placeholder="이메일 주소" 
+          type="email" 
+          icon={<Mailcon />} 
+          name="email"
+          value={email}
+          onChange={onChangeEmail}
+        />
+      </div>
+      <div className="input-wrapper">
+        <Input 
+          placeholder="이름(예: 길동)" 
+          icon={<PersonIcon />} 
+          value={lastname}
+          onChange={onChangeLastname}
+        />
+      </div>
+      <div className="input-wrapper">
+        <Input 
+          placeholder="성(예: 홍)" 
+          icon={<PersonIcon />} 
+          value={firstname}
+          onChange={onChangeFirstname}
+        />
+      </div>
+      <div className="input-wrapper sign-up-password-input-wrapper">
+        <Input 
+          placeholder="비밀번호 설정하기"
+          type={hidePassword ? "password" : "text"}
+          icon={
+            hidePassword ? (
+              <CloseEyeIcon onClick={toggleHidePassword} />
+            ) : (
+              <OpenedEyeIcon onClick={toggleHidePassword}/>
+            )
+          }
+          value={password}
+          onChange={onChangePassword}
+        />
+      </div>
     </Container>
   )
 };
