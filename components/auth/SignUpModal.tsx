d1497211e5bb668156c58352dd054b87dcab2cd2
ghjung <ghjung@simmssoft.com>
2021-04-23 15:12:54 +0900

    10.3.common button component created

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -9,6 +9,7 @@ import CloseEyeIcon from "../../public/static/svg/auth/closed_eye.svg"
 import {dayList, monthList, yearList} from "../../lib/staticData"
 import Selector from "../common/Selector"
 import Input from "../common/Input";
+import Button from "../common/Button"
 
 const Container = styled.div`
   width: 568px;
@@ -75,6 +76,11 @@ const Container = styled.div`
     .sign-up-modal-birthday-year-selector {
       width: 33.3333%;
     }
+  }
+  .sign-up-modal-submit-button-wrapper {
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+    border-bottom: 1px solid ${palette.gray_eb}
   }
 `;
 
@@ -204,6 +210,9 @@ const SignUpModal: React.FC = () => {
             onChange={onChangeBirthYear}
           />
         </div>
+      </div>
+      <div className="sign-up-modal-submit-button-wrapper">
+        <Button type="submit">가입하기</Button>
       </div>
     </Container>
   )
