527281c5cbabf5c00d746e6cecacef95835714ea
ghjung <ghjung@simmssoft.com>
2021-05-12 20:02:46 +0900

    12.3.공통 컴포넌트를 재사용하기(침실개수, 침대개수)

--- components/room/register/RegisterRoomBedrooms.tsx
+++ components/room/register/RegisterRoomBedrooms.tsx
@@ -1,9 +1,13 @@
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { getNumber } from '../../../lib/utils';
 import { useSelector } from '../../../store';
 import { registerRoomActions } from '../../../store/registerRoom';
 import palette from '../../../styles/palette';
 import Counter from '../../common/Counter';
+import Selector from '../../common/Selector';
+import { bedroomCountList } from "../../../lib/staticData";
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -24,24 +28,38 @@ const Container = styled.div`
     max-width: 40px;
     word-break: keep-all;
   }
-
   .register-room-maximum-guest-counter-wrapper {
     width: 320px;
     margin-top: 24px;
     margin-bottom: 32px;
   }
+  .register-room-bedroom-count-wrapper {
+    width: 320px;
+    margin-bottom: 32px;
+  }
+  .register-room-bed-count-wrapper {
+    width: 320px;
+    margin-bottom: 57px;
+  }
 `;
 
 const RegisterRoomBedrooms: React.FC = () => {
-  const maximumGuestCount = useSelector(
-    (state) => state.registerRoom.maximumGuestCount
-  );
+  const maximumGuestCount = useSelector((state) => state.registerRoom.maximumGuestCount);
+  const bedroomCount = useSelector((state) => state.registerRoom.bedRoomCount);
+  const bedCount = useSelector((state) => state.registerRoom.bedCount);
 
   const dispatch = useDispatch();
 
-  const onChangeMaximumGuestCount = (value: number) => {
+  const onChangeMaximumGuestCount = (value: number) => 
     dispatch(registerRoomActions.setMaximumGuestCount(value));
-  };
+
+  // 침실 수 변경 시
+  const onChangeBedroomCount = (event: React.ChangeEvent<HTMLSelectElement>) => 
+    dispatch(registerRoomActions.setBedroomCount(getNumber(event.target.value) || 0));
+  
+  // 침대 개수 변경 시
+  const onChangeBedCount = (value: number) => 
+    dispatch(registerRoomActions.setBedCount(value));
 
   return (
     <Container>
@@ -55,6 +73,22 @@ const RegisterRoomBedrooms: React.FC = () => {
           label="최대 숙박 인원"
           value={maximumGuestCount}
           onChange={onChangeMaximumGuestCount}
+        />
+      </div>
+      <div className="register-room-bedroom-count-wrapper">
+        <Selector
+          type="register"
+          value={`침실 ${bedroomCount}개`}
+          onChange={onChangeBedroomCount}
+          label="게스트가 사용할 수 있는 침실은 몇 개인가요?"
+          options={bedroomCountList}
+        />
+      </div>
+      <div className="register-room-bed-count-wrapper">
+        <Counter 
+          label="침대"
+          value={bedCount}
+          onChange={onChangeBedCount}
         />
       </div>
     </Container>
--- store/registerRoom.ts
+++ store/registerRoom.ts
@@ -55,13 +55,11 @@ const registerRoom = createSlice({
       state.buildingType = action.payload;
       return state;
     },
-
     // 숙소 유형 변경하기
     setRoomType(state, action: PayloadAction<"entire" | "private" | "public">) {
       state.roomType = action.payload;
       return state;
     },
-
     // 게스트용 숙소인지 변경하기
     setIsSetUpForGuest(state, action: PayloadAction<boolean>) {
       state.isSetUpForGuest = action.payload;
@@ -71,7 +69,17 @@ const registerRoom = createSlice({
     setMaximumGuestCount(state, action: PayloadAction<number>) {
       state.maximumGuestCount = action.payload;
       return state;
-    }    
+    },
+    // 침실 개수 변경하기
+    setBedroomCount(state, action: PayloadAction<number>) {
+      state.bedRoomCount = action.payload;
+      return state;
+    },
+    // 최대 침대 개수 변경하기
+    setBedCount(state, action: PayloadAction<number>) {
+      state.bedCount = action.payload;
+      return state;
+    }
   },
 });
 
