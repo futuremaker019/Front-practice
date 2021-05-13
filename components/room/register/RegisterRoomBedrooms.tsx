222f11936c29a83b87e45df4e39ec29051750854
ghjung <ghjung@simmssoft.com>
2021-05-14 01:14:56 +0900

    12.6.숙소 등록하기 2단계 밸리데이션

--- components/room/register/RegisterRoomBedrooms.tsx
+++ components/room/register/RegisterRoomBedrooms.tsx
@@ -8,9 +8,8 @@ import palette from '../../../styles/palette';
 import Counter from '../../common/Counter';
 import Selector from '../../common/Selector';
 import { bedroomCountList } from "../../../lib/staticData";
-import Button from '../../common/Button';
-import RegisterRoomBedTypes from './RegisterRoomBedTypes';
 import RegisterRoomBedList from './RegisterRoomBedList';
+import RegisterRoomFooter from "./RegisterRoomFooter";
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -118,6 +117,7 @@ const RegisterRoomBedrooms: React.FC = () => {
           onChange={onChangeBedroomCount}
           label="게스트가 사용할 수 있는 침실은 몇 개인가요?"
           options={bedroomCountList}
+          isValid={!!bedroomCount}
         />
       </div>
       <div className="register-room-bed-count-wrapper">
@@ -133,6 +133,11 @@ const RegisterRoomBedrooms: React.FC = () => {
         게스트가 잘 파악할 수 있습니다.
       </p>
       <RegisterRoomBedList />
+      <RegisterRoomFooter 
+        prevHref="/room/register/building"
+        nextHref="/room/register/bathroom"
+        isValid={!!bedroomCount}
+      />
     </Container>
   )
 }
