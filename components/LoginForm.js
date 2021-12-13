89e35b0775e83c3e0104fb5ec82c6698e94f3855
futuremaker019 <futuremaker019@gmail.com>
2021-12-13 15:01:32 +0900

    4.6.action과 상태 정리하기

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,8 +4,8 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import useInput from '../hooks/useInput';
-import { useDispatch } from 'react-redux';
-import { loginAction } from '../reducers/user';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginRequestAction } from '../reducers/user';
 
 const ButtonWrapper = styled.div`
 	margin-top: 10px;
@@ -17,6 +17,8 @@ const FormWrapper = styled(Form)`
 
 const LoginForm = () => {
 	const dispatch = useDispatch();
+	const { isLoggingIn } = useSelector((state) => state.user);
+
 	const [id, onChangeId] = useInput('');
 	const [password, onChangePassword] = useState('');
 
@@ -24,7 +26,7 @@ const LoginForm = () => {
 
 	const onSubmitForm = useCallback(() => {
 		console.log(id, password);
-		dispatch(loginAction({ id, password }));
+		dispatch(loginRequestAction({ id, password }));
 	}, [id, password]);
 
 	return (
@@ -32,13 +34,7 @@ const LoginForm = () => {
 			<div>
 				<label htmlFor={'user-id'}>아이디</label>
 				<br />
-				<Input
-					name={'user-id'}
-					type={'text'}
-					value={id}
-					onChange={onChangeId}
-					required
-				/>
+				<Input name={'user-id'} value={id} onChange={onChangeId} required />
 			</div>
 			<div>
 				<label htmlFor={'user-password'}>비밀번호</label>
@@ -54,7 +50,7 @@ const LoginForm = () => {
 			{/*useMemo를 사용하여 리랜더링을 방지한다.*/}
 			{/*<ButtonWrapper style={style}>*/}
 			<ButtonWrapper>
-				<Button type={'primary'} htmlType={'submit'} loading={false}>
+				<Button type={'primary'} htmlType={'submit'} loading={isLoggingIn}>
 					로그인
 				</Button>
 				<Link href={'/signup'}>
--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,19 +1,19 @@
 import React, { useCallback } from 'react';
-import { Avatar, Card, Button } from 'antd';
-import { useDispatch } from 'react-redux';
-import { logoutAction } from '../reducers/user';
+import { Avatar, Button, Card } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutRequestAction } from '../reducers/user';
 
 const UserProfile = () => {
 	const dispatch = useDispatch();
+	const { me, isLoggingOut } = useSelector((state) => state.user);
 
 	const onLogOut = useCallback(() => {
-		dispatch(logoutAction());
+		dispatch(logoutRequestAction());
 	}, []);
 
 	return (
-		<Card>
-			actions=
-			{[
+		<Card
+			actions={[
 				<div key={'twit'}>
 					짹짹
 					<br />0
@@ -27,8 +27,14 @@ const UserProfile = () => {
 					<br />0
 				</div>
 			]}
-			<Card.Meta avatar={<Avatar>ZC</Avatar>} title="ZeroCho" />
-			<Button onClick={onLogOut}>로그아웃</Button>
+		>
+			<Card.Meta
+				avatar={<Avatar>{me?.nickname[0]}</Avatar>}
+				title={me?.nickname}
+			/>
+			<Button onClick={onLogOut} loading={isLoggingOut}>
+				로그아웃
+			</Button>
 		</Card>
 	);
 };
