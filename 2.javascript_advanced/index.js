72901f5ee265c04c8e5de9e074e0290c46472987
futuremaker019 <futuremaker019@gmail.com>
2022-04-18 00:02:23 +0900

    API 호출 연습

--- 2.javascript_advanced/index.js
+++ 2.javascript_advanced/index.js
@@ -1,4 +1,12 @@
-let response = fetch('https://jsonplaceholder.typicode.com/posts').then(
+async function getData() {
+  let rawResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+  let jsonResponse = await rawResponse.json();
+  console.log(jsonResponse);
+}
+
+getData();
+
+let rawResponse = fetch('https://jsonplaceholder.typicode.com/posts').then(
   (res) => {
     console.log(res);
   }
