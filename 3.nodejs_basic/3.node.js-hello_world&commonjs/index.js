1e2f1d8f800cc771e678baa3300a6fb7b2da353f
futuremaker <futuremaker019@gmail.com>
2022-04-18 23:06:34 +0900

    Node practive

---
+++ 3.nodejs_basic/3.node.js-hello_world&commonjs/index.js
@@ -0,0 +1,9 @@
+const calc = require('./calc');
+
+console.log(calc.add(1, 2));
+console.log(calc.add(4, 5));
+console.log(calc.sub(10, 2));
+
+// node ./3.nodejs_basic/index.js 로 실행하여 결과값을 얻는다.
+
+// node.js를 이용하면 모듈 단위로 만들어 사용할 수 있다.
