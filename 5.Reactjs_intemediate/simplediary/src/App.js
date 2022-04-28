157d1ec613d329d45bdc3bbd437c5b114eafdb37
futuremaker <futuremaker019@gmail.com>
2022-04-28 23:07:42 +0900

    React에서 배열 사용하기1 - 리스트 렌더링

--- 5.Reactjs_intemediate/simplediary/src/App.js
+++ 5.Reactjs_intemediate/simplediary/src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: 'futuremaker',
+    content: '하이1',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: '홍길동',
+    content: '하이하이',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: '아무개',
+    content: '하이하이하이',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: 'futuremaker019',
+    content: '하이하이하이하이',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ 5.Reactjs_intemediate/simplediary/src/DiaryList.js
@@ -0,0 +1,24 @@
+import DiaryItem from './DiaryItem';
+
+const DiaryList = ({ diaryList }) => {
+  console.log(diaryList);
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          <DiaryItem key={it.id} {...it} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// defautlProps를 이용하여 props에 어떠한 데이터도 전달이 되지 않았을때
+// 초기화해서 값을 만들어준다.
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
