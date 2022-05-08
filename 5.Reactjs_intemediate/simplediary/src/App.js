1bec1646918a9f3a445fa0d7bb4dc8f7ecfdad48
futuremaker019 <futuremaker019@gmail.com>
2022-05-08 22:17:36 +0900

    React 기본/React에서 API 호출하기

--- 5.Reactjs_intemediate/simplediary/src/App.js
+++ 5.Reactjs_intemediate/simplediary/src/App.js
@@ -1,45 +1,38 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import Lifecycle from './Lifecycle';
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: 'futuremaker',
-//     content: '하이1',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: '홍길동',
-//     content: '하이하이',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: '아무개',
-//     content: '하이하이하이',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 4,
-//     author: 'futuremaker019',
-//     content: '하이하이하이하이',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-// ];
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
 
   // useRef의 초기값 설정을 0으로 한다.
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/comments`
+    ).then((res) => res.json());
+    console.log(res);
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -70,7 +63,6 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
