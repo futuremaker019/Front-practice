d9a434c19072507f90f1ee6bd8af88f19b18b8f0
futuremaker <futuremaker019@gmail.com>
2022-05-10 23:39:30 +0900

    React 기본/최적화1-useMemo

--- 5.Reactjs_intemediate/simplediary/src/App.js
+++ 5.Reactjs_intemediate/simplediary/src/App.js
@@ -1,9 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useMemo } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-
-// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
@@ -15,7 +13,6 @@ function App() {
     const res = await fetch(
       `https://jsonplaceholder.typicode.com/comments`
     ).then((res) => res.json());
-    console.log(res);
 
     const initData = res.slice(0, 20).map((it) => {
       return {
@@ -61,9 +58,25 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const bedCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return { goodCount, bedCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, bedCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {bedCount}</div>
+      <div>기븐 좋을 일기 비율 : {goodRatio}</div>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
