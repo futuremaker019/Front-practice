1dc64d156c16d94589c67b0d96491f9b35058070
futuremaker <futuremaker019@gmail.com>
2022-06-13 23:22:54 +0900

    React app/프로젝트 기초 공사1

--- 6.React_app/emotion-diary/src/App.js
+++ 6.React_app/emotion-diary/src/App.js
@@ -5,20 +5,57 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import RouteTest from './components/RouteTest';
+
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={'App'}
+          leftChild={
+            <MyButton text={'왼쪽 버튼'} onClick={() => alert('왼쪽 클릭')} />
+          }
+          rightChild={
+            <MyButton
+              text={'오른쪽 버튼'}
+              onClick={() => alert('오른쪽 클릭')}
+            />
+          }
+        />
         <h2>App.js</h2>
+
+        {/* process.env.PUBLIC_URL은 public 디렉토리의 위치를 나타낸다. */}
+        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} /> */}
+
+        <MyButton
+          text={'버튼'}
+          onClick={() => alert('버튼 클릭')}
+          type={'positive'}
+        />
+        <MyButton
+          text={'버튼'}
+          onClick={() => alert('버튼 클릭')}
+          type={'negative'}
+        />
+        <MyButton
+          text={'버튼'}
+          onClick={() => alert('버튼 클릭')}
+          type={'default'}
+        />
+        <MyButton
+          text={'버튼'}
+          onClick={() => alert('버튼 클릭')}
+          type={'defdfadfs'}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
